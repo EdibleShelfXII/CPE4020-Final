06a77051bab46ed45b01bc80392da828cd330af2
Luis Rivera <ldr5468@gmail.com>
2022-04-16 16:04:40 -0400

    dotNET now hosts web api at local host:2222. displays sensor data

--- dotNet/CPE4020Final-dotNET/CPE4020Final-dotNET/Form1.cs
+++ dotNet/CPE4020Final-dotNET/CPE4020Final-dotNET/Form1.cs
@@ -10,8 +10,7 @@ using System.Windows.Forms;
 using System.Threading;
 using System.Net;
 using System.Net.Sockets;
-
-
+using System.IO;
 
 namespace CPE4020Final_dotNET
 {
@@ -23,6 +22,9 @@ namespace CPE4020Final_dotNET
         //   luis1 luis2 other1 other2
         string[] sensors = { "", "", "", "" };
 
+        Thread _serverThread = null;
+        TcpListener _listener;
+
         public Form1()
         {
 
@@ -32,7 +34,8 @@ namespace CPE4020Final_dotNET
             button2.Text = "Stop API";
             label1.Text = ("temp sensor 1: null");
             label2.Text = ("temp sensor 2: null");
-            label3.Text = ("API off");        }
+            label3.Text = ("API off");
+        }
 
         delegate void SetChartCallback(string text);
         TcpListener server;
@@ -44,60 +47,41 @@ namespace CPE4020Final_dotNET
         Int32 port = 2222;
         IPAddress localaddr = IPAddress.Parse("10.0.0.220");
 
-        String webAddr = "10.0.0.185:2000";
-
-        private void startServer()
-        {
-            server = new TcpListener(localaddr, port);
-            server.Start();
-            connclient = server.AcceptTcpClient();
-            ns = connclient.GetStream();
-            t = new Thread(DoWork);
-            t.Start();
-        }
-
-        public void DoWork()
-        {
-            byte[] bytes = new byte[256];
-
-            try
-            {
-                //read bytes
-                int bytesRead = ns.Read(bytes, 0, bytes.Length);
-
-                //respond
-                byte[] msg = System.Text.Encoding.ASCII.GetBytes("OK");
-                ns.Write(msg, 0, msg.Length);
-                Console.WriteLine("Sent: {0}", "OK");
-
-                //add temp to chart
-                //this.AddTempPoint(Encoding.ASCII.GetString(bytes, 0, bytesRead));
-
-            }
-
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.ToString());
-
-            }
-
-            connclient.Close();
-            server.Stop();
-            startServer();
-
-        }
+        String webAddr1 = "10.0.0.185:2000";
 
         private void button1_Click(object sender, EventArgs e)
         {
             timer1.Start();
+            Start();
             label3.Text = ("API on");
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
             timer1.Stop();
+            Stop();
             label3.Text = ("API off");
             //client.Dispose();
+        }
+
+        public void Start(int port = 2222)
+        {
+            if (_serverThread == null)
+            {
+                IPAddress ipAddress = new IPAddress(0);
+                _listener = new TcpListener(ipAddress, 2222);
+                _serverThread = new Thread(ServerHandler);
+                _serverThread.Start();
+            }
+        }
+
+        public void Stop()
+        {
+            if (_serverThread != null)
+            {
+                _serverThread.Abort();
+                _serverThread = null;
+            }
         }
 
         private void timer1_Tick(object sender, EventArgs e)
@@ -110,7 +94,7 @@ namespace CPE4020Final_dotNET
             // Download data.
             try
             {
-                byte[] arr = client.DownloadData("http://" + webAddr + "/api/sensor");
+                byte[] arr = client.DownloadData("http://" + webAddr1 + "/api/sensor");
 
                 // Write values.
                 string resp = Encoding.UTF8.GetString(arr);
@@ -158,6 +142,55 @@ namespace CPE4020Final_dotNET
             catch (WebException ee)
             {
 
+            }
+        }
+
+        String ReadRequest(NetworkStream stream)
+        {
+            MemoryStream contents = new MemoryStream();
+            var buffer = new byte[2048];
+            do
+            {
+                var size = stream.Read(buffer, 0, buffer.Length);
+                if (size == 0)
+                {
+                    return null;
+                }
+                contents.Write(buffer, 0, size);
+            } while (stream.DataAvailable);
+            var retVal = Encoding.UTF8.GetString(contents.ToArray());
+            return retVal;
+        }
+
+        void ServerHandler(Object o)
+        {
+            _listener.Start();
+            while (true)
+            {
+                TcpClient client = _listener.AcceptTcpClient();
+                NetworkStream stream = client.GetStream();
+
+                try
+                {
+                    var request = ReadRequest(stream);
+
+                    var responseBuilder = new StringBuilder();
+                    responseBuilder.AppendLine("HTTP/1.1 200 OK");
+                    responseBuilder.AppendLine("Content-Type: text/html");
+                    responseBuilder.AppendLine();
+                    responseBuilder.AppendLine("<html><head><title>CPE4020 Final Project</title></head><body>CPE 4020 Final Project<br>Sensor1: " + sensors[0] + "<br>Sensor2: " + sensors[1] + "</body></html>");
+                    responseBuilder.AppendLine("");
+                    var responseString = responseBuilder.ToString();
+                    var responseBytes = Encoding.UTF8.GetBytes(responseString);
+
+                    stream.Write(responseBytes, 0, responseBytes.Length);
+
+                }
+                finally
+                {
+                    stream.Close();
+                    client.Close();
+                }
             }
         }
 
